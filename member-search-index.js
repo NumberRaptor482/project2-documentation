8232643c436fba03ac947a01e3f34d23454e1395
NumberRaptor482 <davidjeremy.boosi@gmail.com>
2026-03-18 18:28:20 -0500

    Added JavaDoc Web Pages

---
+++ member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"GUI","l":"GUI()","u":"%3Cinit%3E()","k":"3"},{"p":"<Unnamed>","c":"GUI","l":"main(String[])","u":"main(java.lang.String[])","k":"6"}];updateSearchResults();
