04c688114c859b55699d0733a2f506e8974d9678
NumberRaptor482 <davidjeremy.boosi@gmail.com>
2026-03-18 21:26:10 -0500

    Updated documentation

--- member-search-index.js
+++ member-search-index.js
@@ -1,1 +1,1 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"GUI","l":"GUI()","u":"%3Cinit%3E()","k":"3"},{"p":"<Unnamed>","c":"GUI","l":"main(String[])","u":"main(java.lang.String[])","k":"6"}];updateSearchResults();
+memberSearchIndex = [{"p":"<Unnamed>","c":"GUI","l":"GUI()","url":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"GUI","l":"main(String[])","url":"main(java.lang.String[])"}]
--- type-search-index.js
+++ type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"<Unnamed>","l":"GUI"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"GUI"}]
